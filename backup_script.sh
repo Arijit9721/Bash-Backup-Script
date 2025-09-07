2d2ffd605bb0e17149576f2af296f84429397ed3
Arijit9721 <das03285@gmail.com>
2025-09-07 22:50:13 +0530

    initial commit

---
+++ backup_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+
+# needed variables
+  BACKUP_DIR="/backup"
+  ZIP_FILE="$BACKUP_DIR/backup_$(date +%Y%m%d_%H%M%S).zip"
+
+# Check if the /backup folder is present
+  if  ! ls $BACKUP_DIR 2>/dev/null ; then
+    echo "  backup folder not present, creating ..."     
+    mkdir $BACKUP_DIR
+  else
+    echo "backup folder present, continuing the backup process ..." 
+  fi
+
+# Check if the zip utility is installed
+  if ! which zip >/dev/null ; then
+    echo "Zip utility not installed, install to continue"
+    exit 1
+  else
+    echo "Zip utility present ..."
+  fi
+
+# Check if the file to be backed up is provided as a parameter
+  if [ -z $1 ] >/dev/null ; then        # checking if the parameter is provided
+    echo "Please provide the file to be backed up as a parameter when running the script"
+    exit 1
+  elif ! find $1 2>/dev/null ; then     # checking if the file is valid
+    echo "The file is not found, check the entered file"
+    exit 1
+  elif [ ! -s  $1 ] >/dev/null ; then            # checking if the file is empty
+    echo "The file is empty"
+    exit 1
+  else
+    echo "Found the file to backup, starting backup ..."
+  fi
